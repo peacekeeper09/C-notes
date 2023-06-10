5150759bfb121c8b950bf230d8eafa783085970f
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-10 11:48:32 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cout << "Enter first input:" << endl;
+    cin >> a;
+    cout << "Enter second input:" << endl;
+    cin >> b;
+
+    int sum = a + b;
+    cout << "Sum of the provided number is " << sum;
+    return 0;
+}
