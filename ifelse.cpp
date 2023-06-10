ee23eac13bbaa4982fedf9734d821abe425fd7a8
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-10 12:52:26 +0530

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a number: " << endl;
+    cin >> a;
+
+    int b;
+    cout << "Enter second number for comparison: " << endl;
+    cin >> b;
+
+    if (a >b) {
+        cout << "First number entered is greater than the second number provided." << endl;
+
+    }
+
+    else {
+        cout << "The second number is greater than the first number provided. " <<endl;
+    }
+    
+
+}
