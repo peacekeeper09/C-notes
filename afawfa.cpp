4ad5ed8509ce45352d9b23624278a96274c8e8b9
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-10 11:48:10 +0530

    Create afawfa.cpp

---
+++ afawfa.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 100;
+    string b = "Peace";
+    cout << "I am   "<<b <<" my age is" << a <<endl;;
+
+    int c = 100, d = 19, e = 10;
+    cout <<c <<d <<e <<endl;
+
+    const string dogma = "This is a dogma unit terminal.";
+    cout << dogma;
+
+    int rei;
+    cout << "Type a number and press enter:"
+    cin >> rei;
+    cout << "The number you entered is" << rei;
+
+
+    return 0;
+}
