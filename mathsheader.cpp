a747c156d35841d22a0ff2d3736b53c6acca8a8e
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-10 12:35:19 +0530

    Create mathsheader.cpp

---
+++ mathsheader.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int a = 100;
+    int b = sqrt(a);
+    cout << "Square root of a is " << b << endl;
+
+    int c = 13.33;
+    cout << "Round of c is" << round(c);
+    return 0;
+
+}
