849b253675826f6f7918c19f27b026e3e77bce4c
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-11 02:44:01 +0530

    Create herecomesthearray.cpp

---
+++ herecomesthearray.cpp
@@ -0,0 +1,14 @@
+// declaring array and accessing it
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a[4] = {1,2,3,4};;
+    cout << a[2] << endl;
+
+    cout << a[4] << endl;
+
+}
+
