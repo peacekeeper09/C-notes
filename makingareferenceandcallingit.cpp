6a5cce7aed51629b0f16020b385d0bf283ddca8e
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-11 13:24:20 +0530

    Update makingareferenceandcallingit.cpp

--- makingareferenceandcallingit.cpp
+++ makingareferenceandcallingit.cpp
@@ -1,1 +1,12 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string food = "Fish";
+    string &meal = food;
+
+    cout << "Value stored in variable food is " <<food << endl;
+    cout << "Value stored in reference of food is " << meal;
+}
