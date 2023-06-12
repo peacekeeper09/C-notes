96ccd24bf0f62c97baee2cc70c74828c3a961656
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-12 08:16:47 +0530

    Create simplefunctiontest.cpp

---
+++ simplefunctiontest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void FirstFunction() {
+    cout << "This is a testing of my first function in C++!" <<endl;
+    cout <<"Did it work? Can't hear anything from your side so I will assume yes, it worked!" <<endl;
+}
+
+int main() {
+
+    FirstFunction();
+    return 0;
+}
