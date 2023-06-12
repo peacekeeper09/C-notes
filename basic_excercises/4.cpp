1ff5b9f43f9aa9d2bb6820462df407d504dba0cd
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-12 10:03:30 +0530

    Create 4.cpp

---
+++ basic_excercises/4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+
+    cout << "Enter a number: " <<endl;
+    cin >> a;
+
+    cout << "Enter a number: " <<endl;
+    cin >> b;
+    int sum = a + b;
+    cout << "Sum of both number provided is: " << sum << endl;
+}
