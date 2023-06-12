12c4e2c2681f7aa81817d3157072ff4e070cb40b
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-06-12 10:00:28 +0530

    Create 3q.cpp

---
+++ basic_excercises/3q.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()  {
+    cout << "Size of char is " << sizeof(char) <<" byte." <<endl;
+    cout << "Size of short is " << sizeof(short) <<" byte." <<endl;
+    cout << "Size of int is " << sizeof(long) <<" byte." <<endl;
+    cout << "Size of long is " << sizeof(long) <<" byte." <<endl;
+    cout << "Size of long long is " << sizeof(long long) <<" byte." <<endl;
+    cout << "Size of float is " << sizeof(float) <<" byte." <<endl;
+    cout << "Size of double is " << sizeof(double) <<" byte." <<endl;
+    cout << "Size of bool is " << sizeof(bool) <<" byte." <<endl;
+}
